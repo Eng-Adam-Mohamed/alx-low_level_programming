984089832f889256ec11da554507447ef08fb665
Eng-Adam-Mohamed <murad2043@gmail.com>
2022-09-09 00:49:29 -0700

    Updated Heelo Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
- * main = print the string in the put fucntion 
- *
- * Description: usning the main function 
+ * main - print the string in the put fucntion 
+ * Description: using the main function 
  * this program prints "Programming is like building a multilingual puzzle,"
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
