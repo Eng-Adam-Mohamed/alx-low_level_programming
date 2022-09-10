43ad79151bf3edd601c8ae3d0da02aa67a23939d
Eng-Adam-Mohamed <murad2043@gmail.com>
2022-09-10 05:03:07 -0700

    updated prif f file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,9 @@
 
 /**
 * main - print the string in thprintf function
-*
 * Description: using the main function
 * this program prints "with proper grammar, but the outcome is a piece of art,"
-* Return 0
+* Return: 0
 */
 int main(void)
 	{
